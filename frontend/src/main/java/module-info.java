faad3141ff6be9787f04565f56031980d005fc8f
gabost10297 <gabrielaostrowska97@gmail.com>
2026-04-29 18:34:37 +0200

    Zrobilam patcha dodając klasę FileItem do frontendu

--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.frontend {
     requires java.net.http;
     requires com.google.gson;
     requires atlantafx.base;
+    requires org.apache.tomcat.embed.core;
 
     opens com.example.frontend to javafx.fxml, com.google.gson;
     exports com.example.frontend;
